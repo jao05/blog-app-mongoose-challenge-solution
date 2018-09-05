b92350ec499d4d11f27d61ddcdfa96a6c8531e24
Jesse Outlaw <JesseOutlaw03@gmail.com>
2018-09-05 11:50:19 -0400

    updated travis yml, config, serverjs

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 'use strict';
-
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/blog-app-mongoose-challenge-solution-production';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/blog-app-mongoose-challenge-solution';
 exports.PORT = process.env.PORT || 8080;
